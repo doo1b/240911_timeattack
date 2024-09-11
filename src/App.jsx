7abc5a2a973620bdbcaeeb99ed59896a0bca4d7f
doo1b <tngml928@gmail.com>
2024-09-11 12:54:54 +0900

    advaced 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,19 @@ function App() {
     views: "",
   });
 
-  console.log(newPost);
+  const [newComment, setNewComment] = useState({
+    text: "",
+    postId: "",
+  });
 
-  const handleInputChange = (value, key) => {
+  const [selectedPostId, setSelectedPostId] = useState(null);
+
+  const handlePostInputChange = (value, key) => {
     setNewPost({ ...newPost, [key]: value });
+  };
+
+  const handleCommentInputChange = (value) => {
+    setNewComment({ ...newComment, text: value });
   };
 
   const getPosts = async () => {
@@ -22,45 +31,85 @@ function App() {
     return data;
   };
 
+  const getProfile = async () => {
+    const { data } = await axios.get("http://localhost:4000/profile");
+    return data;
+  };
+
+  const getComment = async (postId) => {
+    const { data } = await axios.get(
+      `http://localhost:4000/comments?postId=${postId}`
+    );
+    return data;
+  };
+
   const addPosts = async (post) => {
     await axios.post("http://localhost:4000/posts", post);
   };
 
+  const addComment = async (comment) => {
+    await axios.post("http://localhost:4000/comments", comment);
+  };
+
   const {
     data: posts,
-    isPending: postsPending,
+    isLoading: postsLoading,
     isError: postsError,
   } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
 
-  const { mutate } = useMutation({
+  const {
+    data: profile,
+    isLoading: profileLoading,
+    isError: profileError,
+  } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  const { data: comments, refetch } = useQuery({
+    queryKey: ["comment", selectedPostId],
+    queryFn: ({ queryKey }) => getComment(queryKey[1]),
+    enabled: !!selectedPostId,
+  });
+
+  const { mutate: handleAddPost } = useMutation({
     mutationFn: (newPost) => addPosts(newPost),
     onSuccess: () => {
       queryClient.invalidateQueries(["posts"]);
     },
   });
 
-  if (postsError) return <p>오류가 발생하였습니다...</p>;
-  if (postsPending) return <p>로딩중입니다...</p>;
+  const { mutate: handleAddComment } = useMutation({
+    mutationFn: (newComment) => addComment(newComment),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["comment"]);
+    },
+  });
+
+  if (postsError || profileError) return <p>오류가 발생하였습니다...</p>;
+  if (postsLoading || profileLoading) return <p>로딩중입니다...</p>;
 
   return (
     <>
       <form
         onSubmit={(e) => {
-          e.preventDefault(), mutate(newPost);
+          e.preventDefault(), handleAddPost(newPost);
         }}
       >
         <input
           type="text"
           placeholder="title"
-          onChange={(e) => handleInputChange(e.target.value, "title")}
+          value={newPost.title}
+          onChange={(e) => handlePostInputChange(e.target.value, "title")}
         ></input>
         <input
           type="number"
           placeholder="views"
-          onChange={(e) => handleInputChange(e.target.value, "views")}
+          value={newPost.views}
+          onChange={(e) => handlePostInputChange(e.target.value, "views")}
         ></input>
         <button type="submit">추가</button>
       </form>
@@ -69,6 +118,36 @@ function App() {
           <div key={post.id}>
             <p>{post.title}</p>
             <p>{post.views}</p>
+            <button
+              onClick={() => {
+                refetch();
+                setSelectedPostId(post.id);
+              }}
+            >
+              댓글보기
+            </button>
+            {selectedPostId === post.id && (
+              <div>
+                {comments?.map((comment) => (
+                  <div key={comment.id}>
+                    <p>{comment.text}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleAddComment({ ...newComment, postId: post.id });
+              }}
+            >
+              <input
+                type="text"
+                onChange={(e) => handleCommentInputChange(e.target.value)}
+              />
+              <button type="submit">댓글작성</button>
+            </form>
           </div>
         ))}
       </div>
